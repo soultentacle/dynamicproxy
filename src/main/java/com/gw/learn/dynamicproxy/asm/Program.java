104d8135084b8521e89399e35b9d80ceb9601931
qiaowh <whqiao@wisedu>
2018-09-07 17:59:51 +0800

    Initial commit

---
+++ src/main/java/com/gw/learn/dynamicproxy/asm/Program.java
@@ -0,0 +1,10 @@
+package com.gw.learn.dynamicproxy.asm;
+
+/**
+ * Created by dell on 2018/9/7.
+ */
+public class Program {
+    public void execute(){
+        System.out.println("hello asm");
+    }
+}
